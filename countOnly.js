ccdc42644a5dee9c3d806d2ba93780be80d05670
Abdulbasit79 <Abdulbasit700@outlook.com>
2021-11-24 23:57:15 +0000

    commit

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+
+    if (actual === expected) {
+       console.log(`✅ Assertion Passed: ${actual} ===  ${expected}`);
+     } else if (actual !== expected) {
+       console.log(`🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    }
+   };
+  
+  
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  // allItems: an array of strings that we need to look through
+  // itemsToCount: an object specifying what to count
+  const countOnly = function(allItems, itemsToCount) {
+    const results = {};
+    for (const item of allItems) {
+        if (itemsToCount[item]) { 
+            if (results[item]) {
+                results[item] += 1;
+            } else {
+                results[item] = 1;
+            }
+        }
+    }
+    console.log(results);
+  }
+  
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
