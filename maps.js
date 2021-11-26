39cec7956be085a22f8f56f1d3c0b63deaa93feb
Abdulbasit79 <Abdulbasit700@outlook.com>
2021-11-26 00:00:12 +0000

    commit

--- maps.js
+++ maps.js
@@ -1,15 +1,5 @@
  const words = ["ground", "control", "to", "major", "tom"];
 
-// const map = function (arg1, callback){
-//     let arr = []
-//     for (let index of arg1 ){
-//     arr.push(callback(index))
-//     return arr
-//     }
-// }
-
-// const results1 = map(words, word => word[0]);
-// console.log(results1);
 const map = function(array, callback) {
     const results = [];
     for (let item of array) {
